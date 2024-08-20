bebb862ffe512303efa04f4f7d4186cdb2dc51c0
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-08-20 12:55:20 -0300

    começando a implementar o banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 # a view serve pra renderizar as páginas HTML, que são chamadas de templates
 def index(request):
-  dados = {
-  1: {"nome": "Nebulosa de Carina",
-      "legenda": "webtelescope.org / NASA / James Webb"},
-  2: {"nome": "Galáxia NGC 1079",
-      "legenda": "nasa.org / NASA / Hubble"}
-}
-  
-  return render(request, 'galeria/index.html', {"cards": dados}) # a requisição vai ser sempre o primeiro parametro, em seguida o arquivo HTML a ser renderizado
+  fotografias = Fotografia.objects.all() # traz todos os itens do banco de dados
+  return render(request, 'galeria/index.html', {"cards": fotografias}) # a requisição vai ser sempre o primeiro parametro, em seguida o arquivo HTML a ser renderizado
 
 def imagem(request):
   return render(request, 'galeria/imagem.html')
