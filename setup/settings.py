ec26a21451d1fa6818f75881e6d1ea62d7e71cab
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-08-22 13:51:49 -0300

    Adicionando funcionalidade de upload de imagens

--- setup/settings.py
+++ setup/settings.py
@@ -127,6 +127,12 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') # cria um caminho absoluto pro diretório onde o django vai coletar os arquivos estáticos
 
+# Mídia
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
