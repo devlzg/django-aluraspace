5e4e518e7566689c2becf431341c58ab1690232c
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-08-27 13:34:24 -0300

    Adicionando protótipo da função de login e cadastro

--- setup/urls.py
+++ setup/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls')) # include serve para incluir um template
+    path('', include('galeria.urls')), # include serve para incluir um template
+    path('', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
