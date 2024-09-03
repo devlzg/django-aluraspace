3ae350f0d1e6fb5397c33c7e8cd0b0e14f5ffb96
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-09-03 10:16:03 -0300

    Reorganizando diretórios

--- setup/urls.py
+++ setup/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls')), # include serve para incluir um template
-    path('', include('usuarios.urls'))
+    path('', include('apps.galeria.urls')), # include serve para incluir um template
+    path('', include('apps.usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
